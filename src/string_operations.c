a4461854f2d0e28f364ccfa450b8e9020ce973e3
git-ly <1786066102@qq.com>
2024-06-02 16:34:32 +0800

    Added Makefile and renamed strings.c to string_operations.c

--- src/string_operations.c
+++ src/string_operations.c
@@ -9,6 +9,14 @@ size_t my_strlen(const char *s){
     return (p-s)
 }
 
+char *my_strcpy(char *t,char *s){
+  char *p = t;
+  while(*t++ = *s++)
+  ;
+
+  return p;
+}
+
 int main(void){
     int i;
     char *s[]={
